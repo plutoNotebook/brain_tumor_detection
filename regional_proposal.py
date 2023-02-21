80d32208ae446811e0a12d40a3e146cab33eeea0
plutoNotebook <thom08@naver.com>
2023-02-22 02:40:29 +0900

    02/21

---
+++ regional_proposal.py
@@ -0,0 +1,30 @@
+import cv2
+from iou import get_iou
+
+def selective_search_(ss, img, gtbb, iou_threshold):
+
+    positive_sample = []
+    negative_sample = []
+
+    up_threshold, down_threshold = iou_threshold
+
+    ss.setBaseImage(img) 
+    ss.switchToSelectiveSearchFast() 
+    rects = ss.process()
+
+    for rect in rects:
+        x, y, w, h = rect
+        bb2 = {'x1': x,
+               'y1': y,
+               'x2': x+w,
+               'y2': y+h} 
+        iou = get_iou(gtbb, bb2)
+        img_croped=img[bb2['y1']:bb2['y2'],bb2['x1']:bb2['x2']]
+        
+        if iou >= up_threshold:
+            positive_sample.append(cv2.resize(img_croped, (224, 224)))
+        
+        elif iou <= down_threshold:
+            negative_sample.append(cv2.resize(img_croped, (224, 224)))
+
+    return positive_sample, negative_sample
